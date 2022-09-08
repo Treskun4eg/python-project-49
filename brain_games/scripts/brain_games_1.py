899aa311091193e0c9ea467bc9ab4f5e9da1a3d4
Treskun4eg <wagamamanowelli@gmail.com>
2022-09-08 19:37:03 +0200

    feat: a new name for the file "brain_games_1.py"

---
+++ brain_games/scripts/brain_games_1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from brain_games.cli import hello_user
+
+def main():
+    print("Welcome to the Brain Games!")
+    hello_user()
+
+
+if __name__ == '__main__':
+    main()
+
+
