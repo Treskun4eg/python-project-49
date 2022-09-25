f08e64d9e7caa9eaa1a706222c7dbbbf25499983
Treskun4eg <wagamamanowelli@gmail.com>
2022-09-25 16:21:39 +0200

    feat: add randome_number and your_answer

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Enter shebang to run python."""
 import prompt
+import random
 """
 Import prompt functionality.
 
@@ -10,8 +11,33 @@ Using a Library to Validate Data Input
 
 def welcome_user():
     """Asking for a name of user."""
+    global name
     name = prompt.string('Mai I have your name? ')
     print('Hello, {arg}!'.format(arg=name))
+
+
+def randome_number():
+    global r_n
+    r_n = random.randint(0, 100)
+    print('Question: {arg}'.format(arg=r_n))
+
+
+def your_answer():
+    correct = 'Correct!'
+    answer = prompt.string('Your answer: ')
+    if r_n % 2 != 0 and answer == 'yes':
+        print('\'yes\' is wrong answer ;(. Correct answer was \'no\'.')
+        print('Let\'s try again, {arg}!'.format(arg=name))
+    if r_n % 2 == 0 and answer == 'no':
+        print('\'no\' is wrong answer ;(. Correct answer was \'yes\'.')
+        print('Let\'s try again, {arg}!'.format(arg=name))
+    if answer != 'yes' and answer != 'no':
+        print('Is wrong answer ;(. Correct answer was \'no\' or \'yes\'.')
+        print('Let\'s try again, {arg}!'.format(arg=name))
+    if r_n % 2 == 0 and answer == 'yes':
+        print(correct)
+    if r_n % 2 != 0 and answer == 'no':
+        print(correct)
 
 
 def main():
@@ -19,7 +45,8 @@ def main():
     print('Welcome to the Brain Games!')
     welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    
+    randome_number()
+    your_answer()
 
 
 if __name__ == '__main__':
