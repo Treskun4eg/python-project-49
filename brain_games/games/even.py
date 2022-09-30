570b6f66d55233b1e8fad1a74ec12420d28b1e95
Treskun4eg <wagamamanowelli@gmail.com>
2022-09-30 21:02:03 +0200

    fix: fix bugs

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,8 +1,11 @@
 import random
-"""This module implements pseudo-random number generators for various distributions."""
+import prompt
 
+
+GET_ROUND = 3
 START = 0
 FINISH = 100
+
 
 def randome_number():
     """Generate a random number and check if it's even."""
@@ -12,3 +15,28 @@ def randome_number():
     elif question % 2 != 0:
         correct_answer = 'no'
     return question, correct_answer
+
+
+def your_answer():
+    """
+    Greet the user.
+    Explain the rules of the game
+    Play a game with him
+    """
+    print('Welcome to the Brain Games!')
+    name = prompt.string('Mai I have your name? ')
+    print(f'Hello, {name}!')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    for _ in range(GET_ROUND):
+        question, correct_answer = randome_number()
+        correct = 'Correct!'
+        print(f'Question: {question}')
+        answer = prompt.string('Your answer: ')
+        if answer == correct_answer:
+            print(correct)
+        elif answer != correct_answer:
+            print(f'Is wrong answer {answer};(.')
+            print(f'Correct answer was "{correct_answer}".')
+            print(f'Let\'s try again, {name}!')
+            return
+    print(f'Congratulations, {name}!')
