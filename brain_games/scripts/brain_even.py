917f20ddb2203c9f82e56ec7d3db519eaf64ec84
Treskun4eg <wagamamanowelli@gmail.com>
2022-10-02 12:53:35 +0200

    fix: fix bug brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """Enter shebang to run python."""
-from brain_games.games.even import randome_number
-from brain_games.games.even import your_answer
+from brain_games.games import even
+from brain_games.logic import your_answer
 
 
 def main():
-    randome_number()
-    your_answer()
+    your_answer(even)
 
 
 if __name__ == '__main__':
