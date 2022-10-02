ec20160d6c0a7fd293a2c20b2cfcaf51de865f12
Treskun4eg <wagamamanowelli@gmail.com>
2022-10-02 12:51:04 +0200

    feat: install module logic.py

---
+++ brain_games/logic.py
@@ -0,0 +1,29 @@
+import prompt
+
+
+GET_ROUND = 3
+
+
+def your_answer(game):
+    """
+    Greet the user.
+    Explain the rules of the game
+    Play a game with him
+    """
+    print('Welcome to the Brain Games!')
+    name = prompt.string('Mai I have your name? ')
+    print(f'Hello, {name}!')
+    print(game.DESCRIPTION)
+    for _ in range(GET_ROUND):
+        question, correct_answer = game.randome_number()
+        correct = 'Correct!'
+        print(f'Question: {question}')
+        answer = prompt.string('Your answer: ')
+        if answer == correct_answer:
+            print(correct)
+        elif answer != correct_answer:
+            print(f'Is wrong answer {answer};(.')
+            print(f'Correct answer was "{correct_answer}".')
+            print(f'Let\'s try again, {name}!')
+            return
+    print(f'Congratulations, {name}!')
