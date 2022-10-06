5aae0f9f72575daec3ae3174b6299abdd6504194
Treskun4eg <wagamamanowelli@gmail.com>
2022-10-06 23:28:50 +0200

    feat: launch gcd game

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Enter shebang to run python."""
+from brain_games.games import gcd
+from brain_games.logic import your_answer
+
+
+def main():
+    your_answer(gcd)
+
+
+if __name__ == '__main__':
+    main()
