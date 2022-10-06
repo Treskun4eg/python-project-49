929be62ea1a67057eed77da5e0998a6822bb5c82
Treskun4eg <wagamamanowelli@gmail.com>
2022-10-06 23:26:27 +0200

    feat: add function new game gcd

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,15 @@
+import math
+import random
+
+
+START = 1
+FINISH = 20
+DESCRIPTION = 'Find the greatest common divisor of given numbers.'
+
+
+def current_game():
+    num_1 = random.randint(START, FINISH)
+    num_2 = random.randint(START, FINISH)
+    question = f'{num_1} {num_2}'
+    correct_answer = str(math.gcd(num_1, num_2))
+    return question, correct_answer
