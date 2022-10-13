b0dcbbb29616df642153ba16eff823366ac96bee
Treskun4eg <wagamamanowelli@gmail.com>
2022-10-13 22:51:42 +0200

    feat: add game functionality

---
+++ brain_games/games/prime.py
@@ -0,0 +1,20 @@
+import random
+
+
+START = 1
+START_1 = 2
+FINISH = 100
+DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def current_game():
+    question = random.randint(START, FINISH)
+    q = 0
+    for i in range(START_1, question // 2 + 1):
+        if question % i == 0:
+            q += 1
+    if q <= 0:
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
+    return question, correct_answer
