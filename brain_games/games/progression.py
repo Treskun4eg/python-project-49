7dae61018596420bee0d4ada4d090214ecfc76ee
Treskun4eg <wagamamanowelli@gmail.com>
2022-11-02 19:15:49 +0100

    fix: replacing "for" loop with "join" function

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -11,16 +11,17 @@ index_last = 9
 SEQUENCE_TERM = 10
 
 
-def current_game():
+def return_random_value():
     INITIAL = random.randint(START_1, START_2)
     DIFFERENCE = random.randint(STEP_1, STEP_2)
     LAST_TERM = INITIAL + (SEQUENCE_TERM - 1) * DIFFERENCE
     randome_progression = list(range(INITIAL, LAST_TERM, DIFFERENCE))
     randome_index = random.randint(index_first, index_last)
+    return randome_index, randome_progression
 
-    def replace_value():
-        correct_answer = str(randome_progression[randome_index])
-        randome_progression[randome_index] = '..'
-        question = " ".join(map(str, randome_progression))
-        return question, correct_answer
-    return replace_value()
+
+def current_game(randome_index, randome_progression):
+    correct_answer = str(randome_progression[randome_index])
+    randome_progression[randome_index] = '..'
+    question = " ".join(map(str, randome_progression))
+    return question, correct_answer
