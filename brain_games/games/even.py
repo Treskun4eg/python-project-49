66c68ff0189d141f3ee7da9c720d8309260c96e9
Treskun4eg <wagamamanowelli@gmail.com>
2022-11-10 19:49:45 +0100

    feat: added a function "determine_parity"

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -6,12 +6,16 @@ START = 0
 FINISH = 100
 
 
-def start_round():
+def determine_parity():
     """Generate a random number and check if it's even."""
     question = random.randint(START, FINISH)
     if question % 2 == 0:
-        """Check remainder after division by 2."""
         correct_answer = 'yes'
     elif question % 2 != 0:
         correct_answer = 'no'
+    return question, correct_answer
+
+
+def start_round():
+    question, correct_answer = determine_parity()
     return question, correct_answer
