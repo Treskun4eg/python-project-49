782ae7ea266bf46f0c8da3e15ab00780f5f73fd8
Treskun4eg <wagamamanowelli@gmail.com>
2022-11-10 20:40:04 +0100

    feat: add new function calculate_arguments() and renamed function return_random_value()

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -11,17 +11,22 @@ index_last = 9
 SEQUENCE_TERM = 11
 
 
-def return_random_value():
+def calculate_arguments():
     INITIAL = random.randint(START_1, START_2)
     DIFFERENCE = random.randint(STEP_1, STEP_2)
     LAST_TERM = INITIAL + (SEQUENCE_TERM - 1) * DIFFERENCE
+    return INITIAL, DIFFERENCE, LAST_TERM
+
+
+def generate_progression():
+    INITIAL, DIFFERENCE, LAST_TERM = calculate_arguments()
     randome_progression = list(range(INITIAL, LAST_TERM, DIFFERENCE))
     randome_index = random.randint(index_first, index_last)
     return randome_index, randome_progression
 
 
 def start_round():
-    randome_index, randome_progression = return_random_value()
+    randome_index, randome_progression = generate_progression()
     correct_answer = str(randome_progression[randome_index])
     randome_progression[randome_index] = '..'
     question = " ".join(map(str, randome_progression))
