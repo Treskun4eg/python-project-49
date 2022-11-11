c55c5b2245d537d7d1d0d204f9f96f2c04769f95
Treskun4eg <wagamamanowelli@gmail.com>
2022-11-11 19:27:04 +0100

    fix: renamed variables

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -10,12 +10,12 @@ def start_round():
     num_1 = random.randint(START, FINISH)
     num_2 = random.randint(START, FINISH)
     # submit an operator and get the desired expression
-    list_operations = {
+    sign_to_operation_result = {
         '+': num_1 + num_2,
         '-': num_1 - num_2,
         '*': num_1 * num_2
     }
-    random_expressions = random.choice(list(list_operations.keys()))
-    question = f'{num_1} {random_expressions} {num_2}'
-    correct_answer = str(list_operations[random_expressions])
+    operation_sign = random.choice(list(sign_to_operation_result.keys()))
+    question = f'{num_1} {operation_sign} {num_2}'
+    correct_answer = str(sign_to_operation_result[operation_sign])
     return question, correct_answer
