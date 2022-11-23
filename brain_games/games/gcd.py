d07a57d64888cf4cf94700b80d8751ef05005ed5
Treskun4eg <wagamamanowelli@gmail.com>
2022-11-23 18:14:12 +0100

    fix: allocated a function to get GCD

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -7,9 +7,13 @@ FINISH = 20
 DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
 
+def gcd(num_1, num_2):
+    return math.gcd(num_1, num_2)
+
+
 def get_round():
     num_1 = random.randint(START, FINISH)
     num_2 = random.randint(START, FINISH)
     question = f'{num_1} {num_2}'
-    correct_answer = str(math.gcd(num_1, num_2))
+    correct_answer = gcd(num_1, num_2)
     return question, correct_answer
