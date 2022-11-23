0ce83c49077ab5d5abccc5765c6f87e597b8c815
Treskun4eg <wagamamanowelli@gmail.com>
2022-11-23 18:12:14 +0100

    fix: singled out the parity check function

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -6,16 +6,11 @@ START = 0
 FINISH = 100
 
 
-def check_is_even():
-    """Generate a random number and check if it's even."""
-    question = random.randint(START, FINISH)
-    if question % 2 == 0:
-        correct_answer = 'yes'
-    elif question % 2 != 0:
-        correct_answer = 'no'
-    return question, correct_answer
+def is_even(question):
+    return question % 2 == 0
 
 
 def get_round():
-    question, correct_answer = check_is_even()
+    question = random.randint(START, FINISH)
+    correct_answer = 'yes' if is_even(question) else 'no'
     return question, correct_answer
